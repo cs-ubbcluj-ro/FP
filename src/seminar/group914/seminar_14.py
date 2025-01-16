09f650e18f08c767a4d6926ed6a0debe39a0b05e
Arthur Molnar <arthur486@gmail.com>
2025-01-16 10:33:51 +0200

    Flights problem - what to do

---
+++ src/seminar/group914/seminar_14.py
@@ -0,0 +1,23 @@
+"""
+What code to write for Flights problem
+    - Flight class with properties (only those needed)
+    - FlightRepository (text-file based directly, no inheritance)
+    - FlightService
+    - UserInterface class (implement 1 functionality at a time)
+        - Print all flights when starting the program
+    - PyUnit test for add flight functionality
+        -> class TestAddFlight(TestCase):
+                ...
+
+        -> specification in Service/Repository classes for addFlight function
+            what is a function specification??
+                - what the function does (in human terms) -> NOT how the function does it
+                - input parameters
+                - what it returns (in all cases, so None included)
+                - any exceptions raised?
+
+
+
+
+
+"""
