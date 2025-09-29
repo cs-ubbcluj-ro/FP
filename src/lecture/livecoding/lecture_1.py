8e0c5f280d5969b645f5a6a1b57a736b8a738892
Arthur Molnar <arthur486@gmail.com>
2025-09-29 13:14:17 +0300

    Started code for week 1 livecoding session.

---
+++ src/lecture/livecoding/lecture_1.py
@@ -0,0 +1,9 @@
+"""
+Problem statement:
+    1. Read a string from the program's console
+    2. Divide the string into numbers, words (using space)
+    3. Build a list of primes, a list of negative numbers, a list of the rest of the numbers, a list
+    of capitalized names
+
+    4. Print out all primes numbers etc. in ascending order, without duplicates :)
+"""
