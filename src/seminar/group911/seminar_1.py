4cc0faba5fbf40e51b9d24fef41b977dc97a729c
arthur <arthur@cs.ubbcluj.ro>
2025-09-30 14:50:50 +0300

    Seminar complete :)

--- src/seminar/group911/seminar_1.py
+++ src/seminar/group911/seminar_1.py
@@ -33,20 +33,66 @@ def function_10(a: int, b: int) -> bool:
         return False
 
 
-print(function_10(a, b))
-print(function_10("1", "0"))
+# print(function_10(a, b))
+# print(function_10("1", "0"))
+
 """
 2. Write a Python program which iterates the integers from 1 to 50. 
-    - For multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". 
+    - For multiples of three print "Fizz" instead of the number and for the multiples 
+    of five print "Buzz". 
     - For numbers which are multiples of both three and five print "FizzBuzz".
 """
 
+
+def fizz_buzz():
+    for i in range(1, 51):  # the left argument (1) is included, the right one (51) is excluded
+        if i % 3 == 0 and i % 5 == 0:
+            print(i, "FizzBuzz")  # the print() function takes a variable number of arguments
+        elif i % 3 == 0:  # like the switch statement in C
+            print(i, "Fizz")
+        elif i % 5 == 0:  # like the switch statement in C
+            print(i, "Buzz")
+
+
 """
-3. Write a program that reads temperatures in Celsius from the user (until they enter q), stores them in a list, and then prints:
+3. Write a program that reads temperatures in Celsius from the user (until they enter q), stores them in a list, 
+and then prints:
     - all entered temperatures,
     - the average, minimum, maximum and median temperature,
     - a new list with the values converted to Fahrenheit.
 """
+
+
+# temp = input("Enter temperatures in Celsius:")
+# temp = int(temp)
+# print(type(temp))
+
+def temperatures():
+    temps_list = []  # [] declares an empty Python list
+    # temps_list = list() # call the list class constructor explicitely :)
+
+    while True:
+        value = input("Enter temperature in Celsius: ")
+        if value == "q":
+            break  # stops the innermost currently executing loop
+        temp_value = int(value)
+        temps_list.append(temp_value)
+
+    if len(temps_list) == 0:
+        print("No temperatures entered")
+    else:
+        print("All temperatures: ", temps_list)
+        # / -> real number division (result is a floating point number)
+        # // -> integer number division (result is an int)
+        # sum() - built-in function that returns the sum of the elements
+        # len() - length of its parameter (list, tuple, dict, string, etc.)
+        print("The average: ", sum(temps_list) / len(temps_list))
+        print("The minimum: ", min(temps_list))
+        print("The maximum: ", max(temps_list))
+
+        temps_list.sort()  # sort the list in place, in ascending order
+        print("Median value: ", temps_list[len(temps_list) // 2])
+
 
 """
 4. Given a non-empty string like "Code" return a string like "CCoCodCode"
@@ -54,6 +100,16 @@ print(function_10("1", "0"))
     string_splosion('abc') → 'aababc'
     string_splosion('ab') → 'aab'
 """
+
+
+def splosion(s: str) -> str:
+    splosion = ""
+    for i in range(1, len(s) + 1):
+        splosion += s[0:i]
+    return splosion
+
+print(splosion("Code"))
+
 
 """
 5. Word Frequency Counter
