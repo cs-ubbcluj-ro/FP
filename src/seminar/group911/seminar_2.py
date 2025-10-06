2c5848e24eb15d7075c9b0b32d4614291897dcd2
Arthur Molnar <arthur486@gmail.com>
2025-10-06 12:37:38 +0300

    Starter code for week 2 seminars.

---
+++ src/seminar/group911/seminar_2.py
@@ -0,0 +1,17 @@
+"""
+What we want to do today
+    1. Build a simple menu on the console
+    2. Work with Python's list and dict types
+    3. Generate some random entities in the program (because we don't like to type too much)
+    4. How to structure this small program :)
+
+Problem statement:
+    -- We want to manage a list of cities. Each city has a name, population, country and continent
+What the program needs to do (requirements):
+    1. Sort the cities
+    2. Display the list of cities
+    3. Search for a city (using partial, case-insensitve str matching)
+    4. Add a city from the console
+    5. Add a number of random cities to the list (the number is read from the console)
+    6. Quit
+"""
