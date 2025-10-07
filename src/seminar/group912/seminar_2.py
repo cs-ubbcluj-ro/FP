3c83566efb103d2d3d7a52fae586bc1db4bd7861
arthur <arthur@cs.ubbcluj.ro>
2025-10-07 12:49:06 +0300

    Seminar 2 with group 912 complete.

--- src/seminar/group912/seminar_2.py
+++ src/seminar/group912/seminar_2.py
@@ -24,22 +24,41 @@ What the program needs to do (requirements):
 """
 
 
-def create_city(city_name: str, city_pop: int, city_county: str) -> dict:
+def create_city(city_name: str, city_pop: int, city_county: str) -> list:
     # city represented as a Python dict
     # "city" -> key, city_name -> value (actual city name as str)
-    return {"city": city_name, "population": city_pop, "county": city_county}
+
+    # city as dict
+    # return {"city": city_name, "population": city_pop, "county": city_county}
+
+    # city as list
+    return [city_name, city_pop, city_county]
 
 
-def get_city_name(city: dict) -> str:
-    return city["city"]
+def get_city_name(city: list) -> str:
+    # return city["city"]
+    return city[0]
 
 
-def get_city_pop(city: dict) -> str:
-    return city["population"]
+def get_city_pop(city: list) -> int:
+    # return city["population"]
+    return city[1]
 
 
-def get_city_county(city: dict) -> str:
-    return city["county"]
+def get_city_county(city: list) -> str:
+    # return city["county"]
+    return city[2]
+
+
+# NB! We are not in the user interface part, so we do not use print(), we return the str
+# If we use print(), it gets printed to the console and that is that :(
+# If we return an str, we get to decide later what to do (print, write to file, display on a GUI, etc.)
+def to_str(city: list) -> str:
+    """
+    Return the str representation of a city
+    """
+    return "City name - " + get_city_name(city) + ", population " + str(get_city_pop(
+        city)) + " located in " + get_city_county(city)
 
 
 """
@@ -48,6 +67,12 @@ def get_city_county(city: dict) -> str:
         - In this part we should not have a lot of processing
         (user input/output is hard to test automatically)
 """
+
+
+def display_cities(city_list: list) -> None:
+    print("List of cities:")
+    for city in city_list:
+        print(to_str(city))
 
 
 def print_menu() -> None:
@@ -63,12 +88,29 @@ def start():
     """
     This acts like the main() function of a C program :)
     """
+
+    # let's hard code a few cities
+    city_list = [create_city("Vatra Dornei", 10_000, "Suceava"), create_city("Deva", 89_000, "Hunedoara"),
+                 create_city("Piatra Neamt", 51_000, "Neamt")]
+
+    print(city_list)
+
     while True:
         print_menu()
 
         option = int(input("Enter your choice: "))
         if option == 1:
             continue
+        elif option == 2:
+            display_cities(city_list)
+
+            # print("Locals")
+            # print(locals())
+
+            # print("Globals")
+            # print(globals())
+
+
         elif option == 6:
             # exit the nearest running loop (the while in this case)
             break
