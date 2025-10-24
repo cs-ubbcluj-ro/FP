3494ee0618309bef8f13bfdabeb2c02fa99d463f
Arthur Molnar <arthur486@gmail.com>
2025-10-24 15:02:40 +0300

    Uploaded starter code for week 5 seminars.

---
+++ src/seminar/group911/seminar_5.py
@@ -0,0 +1,34 @@
+"""
+    Dynamic programming
+"""
+
+"""
+    1. Calculating the n-th term of the Fibonacci sequence
+"""
+
+"""
+    2. Calculate the maximum subarray sum (subarray = elements having continuous indices)
+"""
+
+"""
+    3. The knapsack problem. Given the weights and values of N items, put them in a knapsack having capacity W so that 
+    you maximize the value of the stored items. Items can be broken up.
+"""
+
+"""
+    4. 0-1 knapsack problem. Given the weights and values of N items, put them in a knapsack having capacity W so that 
+    you maximize the value of the stored items. Items cannot be broken up (0-1 property).
+"""
+
+"""
+    5. Count in how many ways we can provide change to a given sum of money (N), when provided infinite supplies of the 
+    given coin denominations.
+
+    e.g. Let's say N = 10, and we have coins of values (1, 5, 10); we can give 
+    change in 4 ways (10, 5 + 5, 5 + 1 + ... and 1 + ... + 1)
+"""
+
+"""
+    6. The checkerboard problem
+   https://www.geeksforgeeks.org/gold-mine-problem
+"""
