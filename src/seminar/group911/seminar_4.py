17e88651e2bd3cd5f11715fe6cd00bdec62efde9
arthur <arthur@cs.ubbcluj.ro>
2025-10-27 12:10:35 +0200

    Week 4 seminar code.

--- src/seminar/group911/seminar_4.py
+++ src/seminar/group911/seminar_4.py
@@ -1,6 +1,9 @@
 """
     Problem Solving Methods -- Divide & Conquer, Backtracking
 """
+import random
+
+from src.lecture.examples.ex14_insertion_sort import binary_insertion_sort
 
 """
     1. Implement an optimized version of the merge sort algorithm
@@ -10,9 +13,73 @@
         d. Could merging be further improved using exponential search?
 """
 
+
+# print(sorted([4, 3, 2, 1]))
+# x = sorted
+# print(x)
+#
+# x = 5
+# print(x([4, 3, 2, 1]))
+
+
+# 1. Implement merge sort
+def merge(l1: list, l2: list) -> list:
+    l = []
+    i = 0
+    j = 0
+    while i < len(l1) and j < len(l2):
+        if l1[i] < l2[j]:
+            l.append(l1[i])
+            i += 1
+        else:
+            l.append(l2[j])
+            j += 1
+
+    while i < len(l1):
+        l.append(l1[i])
+        i += 1
+    while j < len(l2):
+        l.append(l2[j])
+        j += 1
+
+    return l
+
+
+def mergesort_better(l: list) -> list:
+    # if len(l) == 1:
+    #     return l
+    if len(l) <= 32:
+        return binary_insertion_sort(l)
+    mid = len(l) // 2
+    left = mergesort_better(l[:mid])
+    right = mergesort_better(l[mid:])
+
+    return merge(left, right)
+
+
+def test_mergesort():
+    for i in range(1, 1000):
+        n = random.randint(1, 1000)
+        arr = random.choices(range(1, 1_000), k=n)
+        arr_copy = arr[:]
+        arr = mergesort_better(arr)
+        arr_copy.sort()
+        if arr != arr_copy:
+            print("The sorting is incorrect.")
+
+
+# test_mergesort()
+
+data = list(range(10, 1, -1))  # the right side of range is excluded
+print(data)
+print(sorted(data))
+print(data)
+data.sort(reverse=True)
+print(data)
+
 """
-    2. Find the smallest number in a list using a recursive divide & conquer implementation. Return None for an empty 
-    list. Implement the following variants:
+2. Find the smallest number in a list using a recursive divide & conquer implementation. Return None for an empty 
+list. Implement the following variants:
         a. Chip & conquer
         b. Divide the list into halves
 """
@@ -71,7 +138,7 @@ def bkt_rec(x, n):
                 bkt_rec(x[:], n)
 
 
-bkt_rec([], 4)
+# bkt_rec([], 4)
 
 """
     6. Change the code above to work for the n-Queen problem
--- src/seminar/group912/seminar_4.py
+++ src/seminar/group912/seminar_4.py
@@ -1,6 +1,8 @@
 """
     Problem Solving Methods -- Divide & Conquer, Backtracking
 """
+import random
+from src.lecture.examples.ex14_insertion_sort import binary_insertion_sort
 
 """
     1. Implement an optimized version of the merge sort algorithm
@@ -9,6 +11,73 @@
         c. Time the resulting version using the ex18_sort_comparison.py example
         d. Could merging be further improved using exponential search?
 """
+
+
+def generate_random(lenght):
+    numbers = []
+    for i in range(lenght):
+        numbers.append(random.randint(1, 1000))
+    return numbers
+
+
+def insertion_sort(data):
+    for i in range(1, len(data)):
+        j = i - 1
+        key = data[i]
+        while j >= 0 and key < data[j]:
+            data[j + 1] = data[j]
+            j -= 1
+        data[j + 1] = key
+    return data
+
+
+def test_sort():
+    for i in range(1000):
+        n = random.randint(1, 1000)
+        list1 = generate_random(n)
+        list2 = list1[:]
+        list3 = list1[:]
+        sorted_list = merge_sort_optimized(list1)
+        sorted_list1 = insertion_sort(list3)
+        list2.sort()
+        if sorted_list != list2:
+            print("Sorting is not ok!")
+        if sorted_list1 != list2:
+            print("Sorting is not ok!")
+
+
+def merge_sort_optimized(data) -> list:
+    if len(data) <= 32:
+        # return data
+        # insertion_sort(data)
+        return binary_insertion_sort(data)
+    else:
+        mid = len(data) // 2
+        right = merge_sort_optimized(data[:mid])
+        left = merge_sort_optimized(data[mid:])
+        return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    return result
+
+
+test_sort()
 
 """
     2. Find the smallest number in a list using a recursive divide & conquer implementation. Return None for an empty 
@@ -71,7 +140,7 @@ def bkt_rec(x, n):
                 bkt_rec(x[:], n)
 
 
-bkt_rec([], 4)
+# bkt_rec([], 4)
 
 """
     6. Change the code above to work for the n-Queen problem
