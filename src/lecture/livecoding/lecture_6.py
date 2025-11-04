3e1430b56357b2465c38ce0cff8c27d316f352c4
arthur <arthur@cs.ubbcluj.ro>
2025-11-04 23:46:46 +0200

    Week 6 lecture examples and starter code.

---
+++ src/lecture/livecoding/lecture_6.py
@@ -0,0 +1,29 @@
+"""
+
+Create a calculator program for rational numbers with the following functionalities:
+    + add a rational number to the calculator
+    u undo the last operation
+    x exit
+"""
+
+
+# ----------------------#
+# Non-UI functions here #
+# ----------------------#
+
+# Functions to handle rational numbers
+
+
+# Functions to handle the calculator
+
+
+# -----------------------#
+# Only UI functions here #
+# -----------------------#
+
+
+def print_menu():
+    print("Calculator menu:")
+    print("   + add a rational number")
+    print("   u undo last operation")
+    print("   x close the calculator")
